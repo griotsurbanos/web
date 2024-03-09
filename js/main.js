cc3ed9e4853f364efb4999ca65ff228f6ea136c3
griotsurbanos <162762011+griotsurbanos@users.noreply.github.com>
2024-03-09 09:17:18 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function loadPage(page) {
+    // Utiliza fetch para cargar el contenido del archivo HTML desde el servidor local
+    fetch(`pages/${page}.html`)
+        .then(response => response.text())
+        .then(html => {
+            // Obtén el contenedor de contenido
+            const contentContainer = document.getElementById('content');
+
+            // Cambia dinámicamente el contenido sin recargar la página
+            contentContainer.innerHTML = html;
+        })
+        .catch(error => console.error('Error al cargar la página:', error));
+}
